0f82aee90dc9113028dadac6797c2f3ee1228da3
AislingHeanue <aislingheanue@gmail.com>
2024-11-24 23:35:31 +0000

    Install rust-analyzer
    Yay rust

--- work/.config/nvim/lua/custom/plugins/lsp-config.lua
+++ work/.config/nvim/lua/custom/plugins/lsp-config.lua
@@ -135,6 +135,7 @@ return {
       yamlls = {},
       jsonls = {},
       bashls = {},
+      rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
--- work/.config/nvim/lua/custom/plugins/treesitter.lua
+++ work/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -15,7 +15,7 @@ return { -- Highlight, edit, and navigate code
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     folding = true,
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go' },
+    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'go', 'rust' },
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
