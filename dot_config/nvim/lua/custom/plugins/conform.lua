f611309a3467bb31d4398f495d2516339bc5bf44
AislingHeanue <aislingheanue@gmail.com>
2026-02-22 21:28:07 +0000

    Fix tmux script bug

--- dot_config/nvim/lua/custom/plugins/conform.lua
+++ dot_config/nvim/lua/custom/plugins/conform.lua
@@ -42,7 +42,8 @@ return { -- Autoformat
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { "gofumpt" },
-      javascript = { "prettierd", "prettier", stop_after_first = true },
+      javascript = { "prettier", stop_after_first = true },
+      typescript = { "prettier", stop_after_first = true },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       -- You can use 'stop_after_first' to run the first available formatter from the list
